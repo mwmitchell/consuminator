316a3fe20af62f219cf9fd4e559e8ebce1200a36
Matt Mitchell <goodieboy@gmail.com>
2009-09-04 17:49:54 -0400

    added docs as root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller=>'docs', :action=>'index'
   map.resources :docs
   map.resources :indexer
+  map.root :controller=>'docs', :action=>:index
 end
